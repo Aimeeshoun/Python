876d42a220649452b3f6df58790ba333652b89d4
aimee <10893245@my.uvu.edu>
2021-11-03 18:48:21 -0600

    working sequence

--- pythonProject/squence2.py
+++ pythonProject/squence2.py
@@ -3,11 +3,11 @@ import maya.cmds as cmds
 sels = cmds.ls(sl=True)
 
 def name_sequence(input):
-    for num in sels:
+    i = 0
+    for i in range(len(sels)):
         string_a = input
         the_string = string_a.partition("##")
         num_pounds = the_string[1].count("#")
-        finished_string = the_string[0] + str(new_sum).zfill(num_pounds) + the_string[2]
+        finished_string = the_string[0] + str(i).zfill(num_pounds) + the_string[2]
         new_name = cmds.rename(finished_string)
-
 name_sequence("Leg_##_Jnt")
